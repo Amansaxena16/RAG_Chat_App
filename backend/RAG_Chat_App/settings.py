2e0237e56c8ef3f1b27526591ebadefba64f468d
Aman Saxena <aman16nov.as@gmail.com>
2026-08-11 13:07:22 +0530

    add postgres as a db

--- backend/RAG_Chat_App/settings.py
+++ backend/RAG_Chat_App/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'RAG_Chat_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rag_chat_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
